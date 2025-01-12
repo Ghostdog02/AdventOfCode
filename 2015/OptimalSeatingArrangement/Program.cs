c4a378ae15b0ce61b26c3a19b4e448f26f4b2fd0
Alexander Vesely <alex.vesely07@gmail.com>
2025-01-12 08:50:43 +0200

    Completed part 1 of day 13

--- 2015/OptimalSeatingArrangement/Program.cs
+++ 2015/OptimalSeatingArrangement/Program.cs
@@ -1,14 +1,18 @@
-﻿namespace OptimalSeatingArrangement
+﻿using System.Collections.Generic;
+using System.IO;
+using System;
+
+namespace OptimalSeatingArrangement
 {
     public class Program
     {
-
-
         static void Main(string[] args)
         {
-            string path = @"D:\Projects\AdventOfCode\2015\OptimalSeatingArrangement\Input.txt";
+            string path = @"D:\Projects\AdventOfCode\2015\OptimalSeatingArrangement\Input2.txt";
             var arrangement = new FindOptimalArrangement();
             arrangement.ProcessInput(path);
+            arrangement.FindOptimalSeatingArrangement();
+            Console.WriteLine(arrangement.TotalHappiness);
         }
 
 
@@ -20,10 +24,13 @@
 
         public List<string> Names { get; }
 
+        public int TotalHappiness { get; private set; }
+
         public FindOptimalArrangement()
         {
             PeopleWithHappiness = new Dictionary<(string, string), int>();
             Names = new List<string>();
+            TotalHappiness = 0;
         }
 
         public void ProcessInput(string path)
@@ -66,7 +73,83 @@
 
         public void FindOptimalSeatingArrangement()
         {
+            GenerateAllPermutationsOfNames(Names.Count - 1);
+        }
 
+        private void GenerateAllPermutationsOfNames(int currentIndex)
+        {
+            if (currentIndex == 1)
+            {
+                CalculateTotalHappiness();
+                return;
+            }
+
+            else
+            {
+                GenerateAllPermutationsOfNames(currentIndex - 1);
+
+                for (int i = currentIndex; i >= 0; i--)
+                {
+                    if (i % 2 == 0)
+                    {
+                        Swap(i, currentIndex);
+                    }
+
+                    else
+                    {
+                        Swap(0, currentIndex);
+
+                    }
+
+                    GenerateAllPermutationsOfNames(currentIndex - 1);
+                }
+
+            }
+        }
+
+        private void Swap(int firstIndex, int secondIndex)
+        {
+            var temp = Names[firstIndex];
+            Names[firstIndex] = Names[secondIndex];
+            Names[secondIndex] = temp;
+        }
+
+        private void CalculateTotalHappiness()
+        {
+            var tempHappiness = 0;
+
+            for (int i = 0; i < Names.Count; i++)
+            {
+                var currentName = Names[i];
+
+                if (i == 0)
+                {
+                    tempHappiness = AddCurrentHappiness(Names[Names.Count - 1], currentName, Names[i + 1], tempHappiness);
+                }
+
+                else if (i == Names.Count - 1)
+                {
+                    tempHappiness = AddCurrentHappiness(Names[i - 1], currentName, Names[0], tempHappiness);
+                }
+
+                else
+                {
+                    tempHappiness = AddCurrentHappiness(Names[i - 1], currentName, Names[i + 1], tempHappiness);
+                }
+            }
+
+            if (tempHappiness > TotalHappiness)
+            {
+                TotalHappiness = tempHappiness;
+            }
+        }
+
+        private int AddCurrentHappiness(string previousName, string currentName, string nextName, int tempHappiness)
+        {
+            tempHappiness += PeopleWithHappiness[(currentName, nextName)];
+            tempHappiness += PeopleWithHappiness[(currentName, previousName)];
+
+            return tempHappiness;
         }
     }
 }
