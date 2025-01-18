4cdac0d995039ef4c916a6cd75f2041ae26f0b0f
Alexander Vesely <alex.vesely07@gmail.com>
2025-01-18 22:08:32 +0200

    Added some minor changes

--- 2015/FindRightBalanceOfIngredients/Program.cs
+++ 2015/FindRightBalanceOfIngredients/Program.cs
@@ -2,27 +2,23 @@
 {
     public class Program
     {
-        //public List<int> numbers = new List<int>();
-
         public static void Main(string[] args)
         {
             var ingredients = new BalancedIngredients();
             //int resultSum = 100;
-            int numbers = 3;
-            int[] partition = [1, 2, 3];
-            //for (int i = 0; i < numbers - 1; i++)
-            //{
-            //    partition[i] = 1;
-            //}
+            int numbers = 4;
+            var partition = new int[numbers];
 
-            //partition[numbers - 1] = resultSum - numbers;
+            for (int i = 0; i < numbers - 1; i++)
+            {
+                partition[i] = 1;                
+            }
 
-            //for (int i = 0; i < numbers; i++)
-            //{
-            //    partition[i] = i + 1;
-            //}
+            partition[numbers - 1] = 97;
             //GeneratePartitions(partition);
-            ingredients.GeneratePermutations(partition, partition.Length);
+            //ingredients.GeneratePermutations(partition, partition.Length);
+            ingredients.GeneratePartitions(partition);
+
         }
 
 
@@ -30,26 +26,44 @@
 
     public class BalancedIngredients
     {
+        public List<int[]> EqualSumNumbers { get; }
+
+        public BalancedIngredients()
+        {
+            EqualSumNumbers = new List<int[]>();
+        }
+
         public void GeneratePartitions(int[] partition)
         {
+            if (partition[partition.Length - 1] == 1)
+            {
+                foreach (int i in partition)
+                {
+                    Console.Write($"{i} ");
+                }
+
+                Console.WriteLine();
+                return;
+            }
+
+            var changedPartition = partition;
+            var lastNumber = changedPartition.Last();
+            changedPartition[changedPartition.Length - 1] = lastNumber - 1;
+            GeneratePermutations(partition, partition.Length);
+
+            GeneratePartitions(changedPartition);
             //var changed = partition;
             //changed[changed.Length - 1]--;
             //GeneratePartitions(changed);
-            GeneratePermutations(partition, partition.Length);
-            //TO DO: Implement Heap's Algorithm and make an base case for recursion
+
+
         }
 
         public void GeneratePermutations(int[] numbers, int size)
         {
             if (size == 1)
             {
-                foreach (var number in numbers)
-                {
-                    Console.Write($"{number} ");
-                }
-
-                Console.WriteLine();
-
+                EqualSumNumbers.Add(numbers);
                 return;
             }
 
@@ -59,7 +73,7 @@
 
                 int swapIndex = (size % 2 == 1) ? 0 : i;
 
-                Swap(swapIndex, size-1, numbers);
+                Swap(swapIndex, size - 1, numbers);
             }
         }
 
