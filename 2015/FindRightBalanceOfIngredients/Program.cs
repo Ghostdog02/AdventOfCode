89a79fc25ac696df18a56cdd17f96f8f37f10f9d
Alexander Vesely <alex.vesely07@gmail.com>
2025-01-20 21:24:36 +0200

    Tried to do iterative approach

--- 2015/FindRightBalanceOfIngredients/Program.cs
+++ 2015/FindRightBalanceOfIngredients/Program.cs
@@ -6,19 +6,20 @@
         {
             var ingredients = new BalancedIngredients();
             //int resultSum = 100;
-            int numbers = 4;
-            var partition = new int[numbers];
+            //int numbers = 4;
+            //var partition = new int[numbers];
 
-            for (int i = 0; i < numbers - 1; i++)
-            {
-                partition[i] = 1;                
-            }
+            //for (int i = 0; i < numbers - 1; i++)
+            //{
+            //    partition[i] = 1;
+            //}
 
-            partition[numbers - 1] = 97;
-            //GeneratePartitions(partition);
-            //ingredients.GeneratePermutations(partition, partition.Length);
-            ingredients.GeneratePartitions(partition);
-
+            //partition[numbers - 1] = 97;
+            ////GeneratePartitions(partition);
+            ////ingredients.GeneratePermutations(partition, partition.Length);
+            ingredients.ReadInput();
+            ingredients.GeneratePartitions();
+            
         }
 
 
@@ -26,60 +27,173 @@
 
     public class BalancedIngredients
     {
-        public List<int[]> EqualSumNumbers { get; }
+        //public List<int[]> CurrentPermutations { get; }
+
+        //public List<int[]> GeneratedSequences { get; }
+
+        public List<int[]> Ingredients { get; }
+
+        public int TotalScore { get; private set; }
 
         public BalancedIngredients()
         {
-            EqualSumNumbers = new List<int[]>();
+            //CurrentPermutations = new List<int[]>();
+            //GeneratedSequences = new List<int[]>();
+            Ingredients = new List<int[]>();
         }
 
-        public void GeneratePartitions(int[] partition)
+        //Generates all sequences which have a sum of 100
+        public void GeneratePartitions()
         {
-            if (partition[partition.Length - 1] == 1)
+            for (int firstCoefficient = 1; firstCoefficient <= 99; firstCoefficient++)
             {
-                foreach (int i in partition)
+                for (int secondCoefficient = 1; secondCoefficient <= 99; secondCoefficient++)
                 {
-                    Console.Write($"{i} ");
+                    for (int thirdCoefficient = 1; thirdCoefficient <= 99; thirdCoefficient++)
+                    {
+                        for (int fourthCoefficient = 1; fourthCoefficient <= 99; fourthCoefficient++)
+                        {
+                            if ((firstCoefficient + secondCoefficient + thirdCoefficient + fourthCoefficient) == 100)
+                            {
+                                var tempScore = 0;
+
+                                tempScore += CalculateScoreOfIngredient(Ingredients[0], firstCoefficient);
+                                tempScore += CalculateScoreOfIngredient(Ingredients[1], secondCoefficient);
+                                tempScore += CalculateScoreOfIngredient(Ingredients[2], thirdCoefficient);
+                                tempScore += CalculateScoreOfIngredient(Ingredients[3], fourthCoefficient);
+
+                                if (tempScore > TotalScore)
+                                    TotalScore = tempScore;
+                            }
+
+                        }
+                    }
+                }
+            }
+        }
+
+        private int CalculateScoreOfIngredient(int[] ingredientProperties, int coefficient)
+        {
+            var tempScore = 0;
+
+            foreach (int property in ingredientProperties)
+            {
+                tempScore += coefficient * property;
+            }
+
+            return tempScore;
+        }
+
+        public void ReadInput()
+        {
+            var path = @"D:\Projects\AdventOfCode\2015\FindRightBalanceOfIngredients\Input.txt";
+            using var reader = new StreamReader(path);
+            string line;
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                int[] numbers = new int[5];
+                
+                var splitLine = line.Split(' ');
+                int index = 0;
+
+                foreach (var property in splitLine)
+                {
+                    if (int.TryParse(property, out int digit))
+                    {
+                        numbers[index] = digit;
+                        index++;
+                    }
                 }
 
-                Console.WriteLine();
-                return;
-            }
-
-            var changedPartition = partition;
-            var lastNumber = changedPartition.Last();
-            changedPartition[changedPartition.Length - 1] = lastNumber - 1;
-            GeneratePermutations(partition, partition.Length);
-
-            GeneratePartitions(changedPartition);
-            //var changed = partition;
-            //changed[changed.Length - 1]--;
-            //GeneratePartitions(changed);
-
-
-        }
-
-        public void GeneratePermutations(int[] numbers, int size)
-        {
-            if (size == 1)
-            {
-                EqualSumNumbers.Add(numbers);
-                return;
-            }
-
-            for (int i = 0; i < size; i++)
-            {
-                GeneratePermutations(numbers, size - 1);
-
-                int swapIndex = (size % 2 == 1) ? 0 : i;
-
-                Swap(swapIndex, size - 1, numbers);
+                Ingredients.Add(numbers);
             }
         }
 
-        private void Swap(int firstIndex, int secondIndex, int[] numbers)
-        {
-            (numbers[secondIndex], numbers[firstIndex]) = (numbers[firstIndex], numbers[secondIndex]);
-        }
+        //public void GeneratePartitions(int[] partition)
+        //{
+        //    //if (partition[partition.Length - 1] == 1)
+        //    //{
+        //    //    foreach (int i in partition)
+        //    //    {
+        //    //        Console.Write($"{i} ");
+        //    //    }
+
+        //    //    Console.WriteLine();
+        //    //    return;
+        //    //}
+
+        //    if (CheckIfNumbersAreEqual(partition) && !GeneratedSequences.Contains(partition))
+        //    {
+        //        partition[partition.Length - 1]--;
+        //        partition[partition.Length - 2]++;
+        //        GeneratedSequences.Add(partition);
+        //        GeneratePartitions(partition);
+        //    }
+
+        //    else
+        //    {
+        //        GeneratePermutations(partition, partition.Length);
+
+        //        foreach (int[] permutation in CurrentPermutations)
+        //        {
+
+        //        }
+
+        //        var changedPartition = partition;
+        //        var lastNumber = changedPartition.Last();
+        //        changedPartition[changedPartition.Length - 1] = lastNumber - 1;
+
+
+        //        GeneratePartitions(changedPartition);
+        //        //var changed = partition;
+        //        //changed[changed.Length - 1]--;
+        //        //GeneratePartitions(changed);
+        //    }
+
+
+
+
+        //}
+
+        //public void GeneratePermutations(int[] numbers, int size)
+        //{
+        //    if (size == 1)
+        //    {
+        //        CurrentPermutations.Add(numbers);
+        //        return;
+        //    }
+
+        //    for (int i = 0; i < size; i++)
+        //    {
+        //        GeneratePermutations(numbers, size - 1);
+
+        //        int swapIndex = (size % 2 == 1) ? 0 : i;
+
+        //        Swap(swapIndex, size - 1, numbers);
+        //    }
+        //}
+
+        //private void Swap(int firstIndex, int secondIndex, int[] numbers)
+        //{
+        //    (numbers[secondIndex], numbers[firstIndex]) = (numbers[firstIndex], numbers[secondIndex]);
+        //}
+
+        //private bool CheckIfNumbersAreEqual(int[] numbers)
+        //{
+        //    bool areEqual = true;
+
+        //    //It checks for all without the last one
+        //    for (int i = 0; i < numbers.Length - 1; i++)
+        //    {
+        //        if (numbers[i] != numbers[i + 1])
+        //        {
+        //            areEqual = false; 
+        //            break;
+        //        }
+        //    }
+
+        //    return areEqual;
+        //}
     }
 }
