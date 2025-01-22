72f507db672a1641a318d929905d6f0f6fb923c6
Alexander Vesely <alex.vesely07@gmail.com>
2025-01-22 13:40:22 +0200

    Completed part two

--- 2015/FindRightBalanceOfIngredients/Program.cs
+++ 2015/FindRightBalanceOfIngredients/Program.cs
@@ -7,19 +7,8 @@ namespace FindRightBalanceOfIngredients
         public static void Main(string[] args)
         {
             var ingredients = new BalancedIngredients();
-            int numbers = 4;
-            var partition = new int[numbers];
-
-            for (int i = 0; i < numbers - 1; i++)
-            {
-                partition[i] = 1;
-            }
-
-            partition[numbers - 1] = 97;
-            ////GeneratePartitions(partition);
-            ////ingredients.GeneratePermutations(partition, partition.Length);
             ingredients.ReadInput();
-            ingredients.GeneratePartitions(partition);
+            ingredients.FindBestRecipe();
             Console.WriteLine(ingredients.TotalScore);
 
         }
@@ -70,98 +59,31 @@ namespace FindRightBalanceOfIngredients
             }
         }
 
-        //Generates all sequences which have a sum of 100
-        public void GeneratePartitions(int[] partition)
+        public void FindBestRecipe()
         {
-            if (partition.Last() == 1)
+            for (int i = 1; i <= 96; i++)
             {
-                CalculateCookieScore(partition);
-
-                return;
-            }
-
-            if (CheckIfNumbersAreEqual(partition) && !CheckTwoCollectionsForEquality(partition))
-            {
-                CalculateCookieScore(partition);
-                GeneratedSequences.Add(partition);
-
-                var partitionCopy = CopyAndAlterArray(partition, 0);
-                GeneratePartitions(partitionCopy);
-            }
-
-            else if (!CheckTwoCollectionsForEquality(partition))
-            {
-                CalculateCookieScore(partition);
-                GeneratedSequences.Add(partition);
-
-                for (int i = 0; i < partition.Length - 1; i++)
+                for (int j = 1; j + i <= 98; j++)
                 {
-                    var partitionCopy = CopyAndAlterArray(partition, i);
-
-                    if (!CheckTwoCollectionsForEquality(partitionCopy))
+                    for (int l = 1; l + j + i <= 99; l++)
                     {
-                        GeneratePartitions(partitionCopy);
-                        GeneratePermutations(partitionCopy, partitionCopy.Length);
-
-                        foreach (var currentPartition in CurrentPermutations)
-                        {
-                            if (!CheckTwoCollectionsForEquality(currentPartition))
-                                GeneratePartitions(currentPartition);
-                        }
+                        int k = 100 - (l + j + i);
+                        CalculateCookieScore(new int[] { i, j, l, k });
                     }
                 }
             }
         }
 
-        private void GeneratePermutations(int[] numbers, int size)
-        {
-            if (size == 1)
-            {
-                if (!CheckTwoCollectionsForEquality(numbers))
-                    CurrentPermutations.Add(numbers);
-
-                return;
-            }
-
-            for (int i = 0; i < size; i++)
-            {
-                GeneratePermutations(numbers, size - 1);
-
-                int swapIndex = (size % 2 == 1) ? 0 : i;
-
-                Swap(swapIndex, size - 1, numbers);
-            }
-        }
-
-        private void Swap(int firstIndex, int secondIndex, int[] numbers)
-        {
-            (numbers[secondIndex], numbers[firstIndex]) = (numbers[firstIndex], numbers[secondIndex]);
-        }
-
-        //It checks for all numbers without the last one
-        private bool CheckIfNumbersAreEqual(int[] numbers)
-        {
-            bool areEqual = true;
-
-            for (int i = 0; i < numbers.Length - 2; i++)
-            {
-                if (numbers[i] != numbers[i + 1])
-                {
-                    areEqual = false;
-                    break;
-                }
-            }
-
-            return areEqual;
-        }
-
         private void CalculateCookieScore(int[] teaspoonsForEachIngredient)
         {
             int tempScore = 0;
+
             int capacity = 0;
             int durability = 0;
             int flavor = 0;
             int texture = 0;
+            //Added variable for part 2
+            int calories = 0;
 
             for (int i = 0; i < Ingredients.Count; i++)
             {
@@ -169,13 +91,10 @@ namespace FindRightBalanceOfIngredients
                 durability += (teaspoonsForEachIngredient[i] * Ingredients[i][1]);
                 flavor += (teaspoonsForEachIngredient[i] * Ingredients[i][2]);
                 texture += (teaspoonsForEachIngredient[i] * Ingredients[i][3]);
+                calories += (teaspoonsForEachIngredient[i] * Ingredients[i][4]);
             }
 
-            //capacity = SetPropertyToZeroIfNegative(capacity);
-            //durability = SetPropertyToZeroIfNegative(durability);
-            //flavor = SetPropertyToZeroIfNegative(flavor);
-            //texture = SetPropertyToZeroIfNegative(texture);
-            if (capacity == 0 || durability == 0 || flavor == 0 || texture == 0)
+            if ((capacity < 0 || durability < 0 || flavor < 0 || texture < 0) || calories != 500)
             {
                 return;
             }
@@ -185,47 +104,5 @@ namespace FindRightBalanceOfIngredients
             if (tempScore > TotalScore)
                 TotalScore = tempScore;
         }
-
-        private int SetPropertyToZeroIfNegative(int property)
-        {
-            return property < 0 ? 0 : property;
-        }
-
-        private bool CheckTwoCollectionsForEquality(int[] numbers)
-        {
-            bool areEqual = false;
-
-            if (GeneratedSequences.Count != 0)
-            {
-                foreach (int[] sequence in GeneratedSequences)
-                {
-                    areEqual = true;
-                    for (int i = 0; i < sequence.Length; i++)
-                    {
-                        if (numbers[i] != sequence[i])
-                        {
-                            areEqual = false;
-                        }
-                    }
-
-                    if (areEqual)
-                        break;
-                }
-            }
-
-            return areEqual;
-            //return !GeneratedSequences.Any(partition => partition.Intersect(numbers).Count() == numbers.Length);   
-        }
-
-        private int[] CopyAndAlterArray(int[] partition, int i)
-        {
-            int[] partitionCopy = new int[partition.Length];
-            Array.Copy(partition, partitionCopy, partition.Length);
-            partitionCopy[partitionCopy.Length - 1]--;
-            partitionCopy[partitionCopy.Length - i - 2]++;
-
-            return partitionCopy;
-        }
-
     }
 }
